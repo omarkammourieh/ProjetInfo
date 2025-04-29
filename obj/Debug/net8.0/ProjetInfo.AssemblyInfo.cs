1d2e1205ccecad5660cc41b83cd7d90bcb5f2d2f
Omar Kammourieh <omar.kammourieh@net.usj.edu.lb>
2025-04-29 22:39:46 +0300

    Ignore .vs/, bin/, obj/ directories

--- obj/Debug/net8.0/ProjetInfo.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetInfo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0948e01cfb12c7b06a7029c9aa1a1bb974e7a7d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d19eab4a5e38e48e12e245da3a3d9d67d5e948e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetInfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
